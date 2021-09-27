a4223f0cf2d75c9a0248cd42a302f099ccfe6a31
Carlos Nascimento <cdgodoy17@gmail.com>
2021-09-27 13:12:55 -0300

    ...

--- src/main/java/br/com/godoydev/Exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/godoydev/Exerciciossb/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.godoydev.Exerciciossb.controllers;
 
 import br.com.godoydev.Exerciciossb.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class ClienteController {
     @GetMapping("/qualquer2")
     public Cliente obterCliente(){
         return new Cliente(01, "Teste", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id){
+        return new Cliente(id, "Maria", "123.456.789-00");
     }
 }
